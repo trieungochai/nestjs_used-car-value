7bf6e525e1dc3a37bbd9e7aca72557e4e3ccc695
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2023-02-05 16:40:22 +0700

    #13 format the Report Response (#2)

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,30 @@
+import { Expose, Transform } from 'class-transformer';
+
+import { User } from 'src/users/user.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  long: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
